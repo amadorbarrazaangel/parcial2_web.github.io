50b5ad5c9225e57f7cee2edf74a3780eb0653731
amadorbarrazaangel <94088706+amadorbarrazaangel@users.noreply.github.com>
2021-11-11 21:40:15 -0700

    Add files via upload

---
+++ Practica javascript/Practica_con_javascript.php
@@ -0,0 +1,22 @@
+  <!DOCTYPE html>
+  <html>
+  <head>
+   
+ <meta charset="utf-8">
+   
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+ <title>  </title>
+    <script src="archivo_javascript.js"></script>
+</head>
+
+
+	<body>
+     <h1>Practica de JS</h1><hr>
+     <div id="contador">0</div>
+     <button onclick="sumaContador()">Suma contador</button>
+
+    <h2>Amador Barraza Angel Isaias</h2>
+
+	</body>
+	</html>
